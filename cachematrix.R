f59c903d3ea8742b0e69bea1151aeab8991a79c9
sophiapelones <86832184+sophiapelones@users.noreply.github.com>
2021-07-09 01:46:16 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix is the primary function of the script.
+## it introduces a new operator, <<- , and contains set, get, setInverse, getInverse.
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        Jeongwoo <- NULL
+        set <- function(y) {
+                x <<- y
+                Jeongwoo <<- NULL
+                }
+        get <- function() {x}
+        
+        setInverse <- function(inverse) {Jeongwoo <<- inverse}
+        getInverse <- function() {Jeongwoo}
+        list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve, the next funciton in the script, solves the inverse of the first function or gets the already solved cached inverse.
 
 cacheSolve <- function(x, ...) {
+        Jeongwoo <- x$getInverse()
+        if(!is.null(Jeongwoo)) {
+                message("getting cached data")
+                return(Jeongwoo)
+                }
+        mat <- x$get()
+        Jeongwoo <- solve(mat, ...)
+        x$setInverse(Jeongwoo)
+        Jeongwoo
+        
         ## Return a matrix that is the inverse of 'x'
 }
